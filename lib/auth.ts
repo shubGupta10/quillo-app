9a536dfb8ead5ecf6870c629308eb9326d5fc81d
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2026-06-05 18:48:19 +0530

    fixed db bug

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,29 +1,33 @@
-import { betterAuth } from "better-auth"
-import { mongodbAdapter } from "better-auth/adapters/mongodb"
-import { MongoClient } from "mongodb"
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-    throw new Error("MONGODB_URI is missing in environment variables. Please add it to your .env.local file.");
+    throw new Error(
+        "MONGODB_URI is missing in environment variables. Please add it."
+    );
 }
 
 const client = new MongoClient(MONGODB_URI);
+
+await client.connect();
+
 const db = client.db();
 
 export const auth = betterAuth({
-
-    //core settup
-    appName: "content-generator",
+    appName: "Quillo",
     baseURL: process.env.BETTER_AUTH_URL,
     secret: process.env.BETTER_AUTH_SECRET,
-
-    database: mongodbAdapter(db),
+    database: mongodbAdapter(db, {
+        client,
+    }),
 
     socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
         },
     },
 });
