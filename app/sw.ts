b0d0eb9b30940dcc67d903a02f4b4932d0a36269
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2026-07-20 15:49:48 +0530

    optimise PWA approach

--- app/sw.ts
+++ app/sw.ts
@@ -1,7 +1,7 @@
 /// <reference lib="webworker" />
 import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist } from "serwist";
+import type { PrecacheEntry, SerwistGlobalConfig, RuntimeCaching } from "serwist";
+import { Serwist, StaleWhileRevalidate } from "serwist";
 
 declare global {
     interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -11,12 +11,22 @@ declare global {
 
 declare const self: ServiceWorkerGlobalScope;
 
+const instantPageCache: RuntimeCaching[] = [
+    {
+        matcher: ({ request }) => request.mode === "navigate",
+        handler: new StaleWhileRevalidate({
+            cacheName: "page-cache",
+        }),
+    },
+    ...defaultCache,
+]
+
 const serwist = new Serwist({
     precacheEntries: self.__SW_MANIFEST,
     skipWaiting: true,
     clientsClaim: true,
     navigationPreload: true,
-    runtimeCaching: defaultCache,
+    runtimeCaching: instantPageCache,
 });
 
 serwist.addEventListeners();
