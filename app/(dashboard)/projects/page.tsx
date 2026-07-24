c8e5ad94c94eac6adb3187613aceb3041c861cd2
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2026-07-24 13:01:37 +0530

    added new projects button

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -2,13 +2,8 @@ import { ProjectList } from "@/features/projects/components/project-list";
 import { getProjects } from "@/features/projects/actions/get-projects";
 
 export default async function ProjectsPage() {
-
   const response = await getProjects();
   const projects = response.success && response.data ? response.data : [];
 
-  return (
-    <div className="space-y-8">
-      <ProjectList projects={projects} />
-    </div>
-  );
+  return <ProjectList projects={projects} />;
 }
