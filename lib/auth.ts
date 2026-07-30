542c6f9d6bcc508cf397ba1017fb776ade88050b
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2026-07-30 13:05:13 +0530

    fixed bug into auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,12 +3,13 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb"
 import { client } from "./mongodb-client"
 import { sendEmail } from "./email/mailer";
 import { getWelcomeEmailHtml } from "./email/templates";
+import { connectDB } from "./db";
+import Auth from "@/features/auth/model/auth.model";
 
 const db = client.db();
 
 export const auth = betterAuth({
 
-    //core settup
     appName: "Quillo",
     baseURL: process.env.BETTER_AUTH_URL,
     secret: process.env.BETTER_AUTH_SECRET,
@@ -27,13 +28,24 @@ export const auth = betterAuth({
             create: {
                 after: async (user) => {
                     try {
+                        await connectDB();
+                        await Auth.create({
+                            authUserId: user.id,
+                            fullName: user.name,
+                            email: user.email,
+                            image: user.image,
+                            createdAt: new Date(),
+                            lastSeenAt: new Date(),
+                            onboardingCompeleted: false,
+                        });
+
                         await sendEmail({
                             to: user.email as string,
                             subject: "Welcome to Quillo!",
                             html: getWelcomeEmailHtml(user.name as string)
-                        })
+                        });
                     } catch (error) {
-                        console.error("Failed to send welcome email:", error);
+                        console.error("Failed to execute user creation hooks:", error);
                     }
                 }
             }
